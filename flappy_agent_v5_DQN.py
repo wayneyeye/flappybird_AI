1932545ee56f7028f3257e6fdb62894cfab3decd
Wenhe Ye <yewenhe0904@gmail.com>
2018-05-08 00:09:06 -0500

    add dqn model 4 inputs

--- flappy_agent_v5_DQN.py
+++ flappy_agent_v5_DQN.py
@@ -2,13 +2,13 @@
 from random import choice, random
 import tensorflow as tf
 import numpy as np
+from sklearn.utils import shuffle
 
 ## define the tf network here
 tf.reset_default_graph()
 n_inputs=4
 n_hidden1=10
 n_hidden2=10
-n_hidden3=5
 n_outputs=2
 initializer=tf.contrib.layers.variance_scaling_initializer()
 #the network
@@ -36,28 +36,43 @@ class flappy_agent():
 		# DQN hyper parameters
 		self.iteration=0
 		self.game_number=0
-		self.n_iterations=2000 # after which the epsilon is forced to zero
+		self.n_iterations=10000 # after which the epsilon is forced to zero
 		self.n_max_step=10000
-		self.n_games_per_update=5
+		self.n_games_per_update=10
 		self.save_per_iterations=100
-		self.sample_interval=4
-		self.network_learning_rate=0.3
-		self.discount_rate=0.97
-		self.policy_learning_rate=0.5
+		self.sample_interval=8
+		self.discount_rate=0.95
 		self.sess=tf.Session()
-		self.epsilon=1
-		self.epsilon_decay=0.01
-		self.network_decay=0.01
-		self.policy_decay=0.01
-		self.flap_rate=0.2
+		self.epsilon=0.5
+		self.epsilon_decay=0.1
+		self.network_learning_rate=0.05
+		self.min_network_learning_rate=0.001
+		self.network_decay=0.1
+		# self.policy_learning_rate=0.3
+		# self.policy_decay=0.01
+		self.flap_rate=0.4
+		self.boosting_coef=0
 
 		# DQN algorithm feeds
-		self.all_actions=[] #
 		self.reward_rate=0.1
+		self.punishment=0
+		self.all_actions=[] #
 		self.all_rewards=[] #
 		self.all_obs=[] #
 		self.all_current_qsa=[]
 
+		# DQN boosting
+		self.round_rewards=[]
+		self.round_obs=[]
+		self.round_current_qsa=[]
+		self.round_actions=[]
+
+		# DQN batch
+		self.batch_size=5
+		self.n_epochs=3
+
+
+		# init network and logger
 		self.sess.run(init)
 		self.max_score=0
 		self.max_score_log=[]
@@ -73,46 +88,51 @@ class flappy_agent():
 	def new_round(self):
 		'''initialize before each game'''
 		self.game_number+=1
+		self.round_rewards=[]
+		self.round_obs=[]
+		self.round_current_qsa=[]
+		self.round_actions=[]
 		
 
 	def update_model(self):
 		'''optimize the tf network every fixed intervals'''
-		# covenrt obs array
-		next_obs_array=np.array(self.all_obs[1:])
-		# process in batch to speed up
-		next_q_values_array=self.sess.run([q_values],feed_dict={X:next_obs_array})[0]
-		# calculate q_target
-		target_q_values_list=self.all_current_qsa.copy()
-		# print(target_q_values_list)
-		index=0
-		policy_learning_rate=max(0.05,self.policy_learning_rate/(1+self.iteration*self.policy_decay))
-		for q,a,r in zip(self.all_current_qsa,self.all_actions,self.all_rewards):
-			q_next_list=list(next_q_values_array[index,:])
-			# if not survived
-			if r<self.reward_rate:
-				q_next_list=[0,0]
-			q_new=(1-policy_learning_rate)*q[a]+policy_learning_rate*(r+self.discount_rate*max(q_next_list))
-			# modify only the q(s,a) that has been executed
-			target_q_values_list[index][a]=q_new
-			index+=1
-			if index==next_q_values_array.shape[0]-1:
-				target_q_values_list[index][a]=target_q_values_list[index][a]*(1-policy_learning_rate)+policy_learning_rate*r
-				break
-		# convert to numpy array
-		target_q_values_array=np.array(target_q_values_list)
-
-		
-		# DQN Training ops
 		if self.game_number%self.n_games_per_update==0:
-			if self.iteration==5:
+		# process in batch to speed up
+			next_q_values_array=np.array(self.all_current_qsa[1:])
+			# calculate q_target
+			target_q_values_list=self.all_current_qsa.copy()
+			# print(target_q_values_list)
+			index=0
+			# policy_learning_rate=max(0.05,self.policy_learning_rate/(1+self.iteration*self.policy_decay))
+			for q,a,r in zip(self.all_current_qsa,self.all_actions,self.all_rewards):
+				# if not survived
+				if r<self.reward_rate:
+					q_next_list=[0,0]
+				else:
+					q_next_list=list(next_q_values_array[index,:])
+				q_new=r+self.discount_rate*max(q_next_list)
+				# modify only the q(s,a) that has been executed
+				target_q_values_list[index][a]=q_new
+				index+=1
+				
+			# convert to numpy array
+			target_q_values_array=np.array(target_q_values_list)
+			obs_X=np.array(self.all_obs)
+		
+			if self.iteration==100:
 				print(self.all_rewards)
 				print(self.all_actions)
 				print(target_q_values_list)
 				print(q_next_list)
 
-			current_lr=max(0.05,self.network_learning_rate/(1+self.iteration*self.network_decay))
-			feed_dict={X:np.array(self.all_obs),q_target:target_q_values_array,learning_rate:current_lr}
-			self.sess.run(training_op,feed_dict=feed_dict)
+			current_lr=max(self.min_network_learning_rate,self.network_learning_rate/(1+self.iteration*self.network_decay))
+			for epoch in range(self.n_epochs):
+				obs_X,target_q_values_array=shuffle(obs_X,target_q_values_array)
+				for i in range(obs_X.shape[0]//self.batch_size):
+					obs_batch=obs_X[i*self.batch_size:i*self.batch_size+self.batch_size]
+					target_q_batch=target_q_values_array[i*self.batch_size:i*self.batch_size+self.batch_size]
+					feed_dict={X:obs_batch,q_target:target_q_batch,learning_rate:current_lr}
+					self.sess.run(training_op,feed_dict=feed_dict)
 			# reset all records after model update
 			self.all_actions=[] #
 			self.all_rewards=[] #
@@ -136,6 +156,7 @@ class flappy_agent():
 		feed_dict={X:obs.reshape(1,n_inputs)})
 		#record current qsas
 		self.all_current_qsa.append(q_val[0][0])
+		self.round_current_qsa.append(q_val[0][0])
 		# get the best action (epsilon greedy)
 		if self.iteration<=self.n_iterations:
 			if random()<self.epsilon/(1+self.iteration*self.epsilon_decay):
@@ -151,9 +172,10 @@ class flappy_agent():
 				best_action=1
 			else:
 				best_action=0
-		# record the estimated q_val
+		# record the actions
 		
 		self.all_actions.append(best_action)
+		self.round_actions.append(best_action)
 		return best_action
 
 	def next_step(self,state):
@@ -161,11 +183,21 @@ class flappy_agent():
 		obs=self.stateWrapper(state)
 		# survive=1 
 		self.all_rewards.append(self.reward_rate)
+		self.round_rewards.append(self.reward_rate)
 		self.all_obs.append(obs)
+		self.round_obs.append(obs)
 
-	def end_round(self):
+	def end_round(self,score):
 		# set the last reward to ...
-		self.all_rewards[-1]=-3    
+		self.all_rewards[-1]=self.punishment    
+		self.round_rewards[-1]=self.punishment
+		# boosting the high score
+		for i in range(score*self.boosting_coef):
+			self.all_actions=self.all_actions+self.round_actions
+			self.all_rewards=self.all_rewards+self.round_rewards
+			self.all_obs=self.all_obs+self.round_obs
+			self.all_current_qsa=self.all_current_qsa+self.round_current_qsa
+
 
 	def debug(self):
 		'''performance information send to stdout'''
@@ -182,11 +214,12 @@ class flappy_agent():
 		self.last_100_avg_log.append(sum(self.last_100)/len(self.last_100))
 		if self.iteration % self.save_per_iterations==0:
 			# saver.save(self.sess,self.model_checkpoints_path)
+			pass
 
 
 	def stateWrapper(self,state):
 		'''simple division added to ensure the range of the input space'''
-		return np.array([state['playery']/1000,state['pipeY']/1000,state['playerVelY']/20,state['pipeRightPos']/512])
+		return np.array([state['playery']/500,state['pipeY']/500,state['playerVelY']/10,state['pipeRightPos']/200])
 	
 	def get_state(self,state,reduce_factor=1):
 		playerLeftPos=state['playerx']
